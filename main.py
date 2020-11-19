e3b03e6fd18ece455a0a7149191d0f8c0da7f2be
Chema-a <jose.davalos4980@alumnos.udg.mx>
2020-11-19 00:24:05 -0600

    Actividad CRUD II - Actualización, Eliminacion y Consulta de Peliculas de un Usuario

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
-from conexion import get_usuario, crear_usuario, iniciar_sesion, insertar_pelicula, get_peliculas, get_pelicula
+from conexion import get_usuario, crear_usuario, iniciar_sesion, insertar_pelicula, get_peliculas, get_pelicula, modificar_pelicula, eliminar_pelicula, get_peliculas_usuario
 app = Flask(__name__)
 
 @app.route("/api/v1/usuarios", methods=["POST"])
-def usuario():
+@app.route("/api/v1/usuarios/<int:id>/peliculas", methods=["GET"])
+def usuario(id = None):
     if request.method == "POST" and request.is_json:
         try:
             data = request.get_json()
@@ -14,8 +15,10 @@ def usuario():
                 return jsonify({'code': 'existe'})
         except:
             return jsonify({"code": "error"})
+    elif request.method == "GET" and id is not None:
+        return jsonify(get_peliculas_usuario(id)) 
 @app.route("/api/v1/peliculas", methods=["GET","POST"])
-@app.route("/api/v1/peliculas/<int:id>", methods=["GET"])
+@app.route("/api/v1/peliculas/<int:id>", methods=["GET","PATCH", "DELETE"])
 def peliculas(id= None):
     if request.method == "POST" and request.is_json:
         try:
@@ -31,6 +34,20 @@ def peliculas(id= None):
         return jsonify(get_peliculas())
     elif request.method == "GET" and id is not None:
         return jsonify(get_pelicula(id)) 
+    elif request.method == "PATCH" and id is not None and request.is_json:
+        data = request.get_json()
+        columna = data['columna']
+        valor = data['valor']
+        if modificar_pelicula(id, columna,valor):
+            return jsonify(code = 'ok')
+        else:
+            return jsonify(code = 'error')
+    elif request.method == "DELETE" and id is not None:
+        if eliminar_pelicula(id):
+            return jsonify(code = 'ok')
+        else:
+            return jsonify(code = 'error')
+
 @app.route("/api/v1/sesiones", methods = ["POST"])
 def sesion():
     if request.method == "POST" and request.is_json:
