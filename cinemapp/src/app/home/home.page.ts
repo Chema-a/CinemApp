5fb9c30a4987a541e855e69a984d8fa0aaf2cd44
Chema-a <jose.davalos4980@alumnos.udg.mx>
2020-11-29 23:46:42 -0600

    Actividad 17 - Ionic(I)

---
+++ cinemapp/src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../api/peliculas.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+  peliculas = [];
+
+  constructor(private peliculasService: PeliculasService) {}
+  ngOnInit(){
+    this.peliculasService.peliculas.subscribe(peliculas =>{
+     this.peliculas = peliculas; 
+    });
+    this.peliculasService.getPeliculas();
+  }
+
+}
